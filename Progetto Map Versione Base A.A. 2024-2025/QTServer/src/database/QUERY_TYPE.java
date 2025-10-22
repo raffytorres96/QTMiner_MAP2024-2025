4d42d5a63f99eda4f109d9e5997bc851c69c6e83
nicola <n.lastella2@studenti.uniba.it>
2025-10-22 18:11:30 +0200

    Aggiunti Javadoc a database

--- Progetto Map Versione Base A.A. 2024-2025/QTServer/src/database/QUERY_TYPE.java
+++ Progetto Map Versione Base A.A. 2024-2025/QTServer/src/database/QUERY_TYPE.java
@@ -1,5 +1,18 @@
 package database;
 
+/**
+ * Enumera i tipi di query di aggregazione supportate.
+ * Specifica se l'utente è interessato al valore minimo (MIN)
+ * o al valore massimo (MAX) di un attributo.
+ */
 public enum QUERY_TYPE {
-    MIN, MAX
+    /**
+     * Identifica una query per la ricerca del valore minimo.
+     */
+    MIN,
+
+    /**
+     * Identifica una query per la ricerca del valore massimo.
+     */
+    MAX
 }
