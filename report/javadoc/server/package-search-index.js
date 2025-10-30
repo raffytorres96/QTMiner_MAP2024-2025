974f564971067074ac438615757240b97f80f805
raffytorres96 <torres96_zx@hotmail.it>
2025-10-30 19:34:22 +0100

    Riaggiornati tutti gli javadoc presenti

--- report/javadoc/server/package-search-index.js
+++ report/javadoc/server/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"data"},{"l":"database"},{"l":"keyboardinput"},{"l":"mining"},{"l":"server"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"data"},{"l":"database"},{"l":"mining"},{"l":"server"}];updateSearchResults();
